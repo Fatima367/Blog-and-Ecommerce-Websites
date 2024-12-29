b01a652adbf4ceedd5c207846d0f81bf7ddab2f7
Fatima367 <princess.fatima3275@gmail.com>
2024-12-29 20:45:51 +0500

    updates

--- blog-website/next.config.ts
+++ blog-website/next.config.ts
@@ -8,16 +8,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "placehold.co" },
     ],
   },
-  webpack(config, { isServer }) {
-    // Ignore the content/page.tsx during server-side compilation
-    if (isServer) {
-      config.module.rules.push({
-        test: /content\/page\.tsx$/,
-        loader: 'ignore-loader',
-      });
-    }
-    return config;
-  },
+ 
 };
 
 export default nextConfig;
